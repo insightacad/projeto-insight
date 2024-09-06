6701b36cbe8d162e1e4eded5e2c200471c6d4ff8
Berlim <106534083+021berlim@users.noreply.github.com>
2024-09-06 09:59:12 -0300

    Versão 1.0.16

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,17 +3,15 @@ const path = require('path');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-    // Rotas de cadastro e login
+    // Rotas GET de autenticação 
     router.get('/signup', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/pages/signup.html'));
     });
-  
-    router.post('/signup', authController.signup);
-  
     router.get('/signin', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/pages/signin.html'));
     });
-  
-    router.post('/signin', authController.signin);
+    // Rotas POST de autenticação
+    router.post('/api/signup', authController.signup);
+    router.post('/api/signin', authController.signin);
 
 module.exports = router;
