ecf29ed849c122ee2ba2c0e420ded0459adecf8b
Berlim <djtegegames@gmail.com>
2024-09-15 21:55:34 -0300

    Versão 1.1.1

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const blockedRoutes = require('./routes/blockedRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 const { eAdmin } = require('./middlewares/auth');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 
 // Configurar o caminho até a pasta styles localizada em frontend
@@ -17,11 +19,16 @@ app.use(cookieParser()); // Adiciona o middleware cookie-parser
 // Usar a variável de ambiente PORT ou um valor padrão
 const port = process.env.PORT ;
 
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/insightacad.com.br/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/insightacad.com.br/fullchain.pem')
+};
+
 // ROTAS
 app.use('/',getRoutes);
 app.use('/dashboard',eAdmin,blockedRoutes);
 app.use('/api',apiRoutes)
 
-app.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}: http://localhost:${port}`);
+https.createServer(options, app).listen(port, () => {
+    console.log(`Servidor iniciado na porta ${port}: https://localhost:${port}`);
 });
